fb3274c4fb5e902a25ccaf7da367b2050b6e027f
mjbejarano <mjbejaranob@gmail.com>
2019-05-13 22:53:34 +0800

    Update index.js
    funtion to create tables added

--- index.js
+++ index.js
@@ -15,8 +15,29 @@ db.connect((err) => {
     }
     console.log('MySql Connected...');
 });
-
 const index = express();
+// Create table
+index.get('/createjobtable', (req, res) => {
+    let sql = 'CREATE TABLE Jobs(id int AUTO_INCREMENT, Job VARCHAR(255), Client VARCHAR(255), Name VARCHAR(255), State VARCHAR(255), Start VARCHAR(255), Due VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
+});
+
+// Insert Job Into the Data Base
+//app.get('/Add Job', (req, res) => {
+ //   let post = {title:'Post One', body:'This is post number one'};
+   // let sql = 'INSERT INTO posts SET ?';
+  //  let query = db.query(sql, post, (err, result) => {
+  //      if(err) throw err;
+   //     console.log(result);
+    //    res.send('Post 1 added...');
+ //   });
+//});
+
+
 
 index.listen('3000', () => {
     console.log('Server started on port 3000');
