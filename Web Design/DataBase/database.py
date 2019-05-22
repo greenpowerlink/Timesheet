b3971d6c406ffd02616c47b927e7b3602e07d610
DanyCaro <50565029+DanyCaro@users.noreply.github.com>
2019-05-22 14:39:15 +0800

    does not work yet

--- Web Design/DataBase/database.py
+++ Web Design/DataBase/database.py
@@ -3,7 +3,7 @@ import MySQLdb
 
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
-                     password="TeckGlobal",  # your password
+                     passwd="TeckGlobal",  # your password
                      db="teckglobal")        # name of the data base
 
 # you must create a Cursor object. It will let
@@ -15,6 +15,6 @@ cur.execute("SELECT * FROM employee")
 
 # print all the first cell of all the rows
 for row in cur.fetchall():
-    print row[0]
+    print (row[0]);
 
 db.close()
--- Web Design/PHP/index.php
+++ Web Design/PHP/index.php
@@ -8,6 +8,19 @@ include_once 'includes/dbh.php';
   <title></title>
 </head>
 <body>
+  <?php
+   $sql = "SELECT * FROM employee;";
+   $result = mysqli_query($conn, $sql);
+   $resultCheck = mysqli_num_rows($result);
+
+   if ($resultCheck > 0){
+     while($row = mysqli_fetch_assoc($result)){
+        echo $row['employe_emp_id']. "<br>";
+     }
+
+   }
+
+  ?>
 
 
 </body>
