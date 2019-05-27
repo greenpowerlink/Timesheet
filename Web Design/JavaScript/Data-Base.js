527df0d8a365855727bdef92abf3f368a4656244
DanyCaro <50565029+DanyCaro@users.noreply.github.com>
2019-05-27 16:07:10 +0800

    created an updated TB
    i downloaded libraries to create the database
    Now we are able to fetch and modidy data for some reason the previos one did not work

--- Web Design/JavaScript/Data-Base.js
+++ Web Design/JavaScript/Data-Base.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    // password: '123456',
+    password: '12345',
     database: 'teckglobal'
 
 });
@@ -19,32 +19,64 @@ db.connect((err) => {
     console.log('MySql Connected...');
 });
 
-
-
-
-/* 
 const app = express();
-// Create table
-function sqlcreation() {
-    sql = 'CREATE TABLE Jobs(id int AUTO_INCREMENT, Job VARCHAR(255), Client VARCHAR(255), Name VARCHAR(255), State VARCHAR(255), Start VARCHAR(255), Due VARCHAR(255), PRIMARY KEY(id))';
+
+
+//create db   http://127.0.0.1:3000/createdb
+
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE teckglobal';
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
-
-        res.send('Posts table created...');
+        res.send('Database created...')
     });
-    data = document.getElementById("firstname").value;
-    document.getElementById('test2').textContent = data;
-}
-// Select posts
-app.get('/getposts', (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+});
+
+// create table  http://127.0.0.1:3000/createemployeetable
+
+app.get('/createemployeetable', (req, res) => {
+    let sql = 'CREATE TABLE employeeList(emp_id int NOT NULL AUTO_INCREMENT, first_name VARCHAR(25), last_name VARCHAR(25), emailAddress VARCHAR(40) UNIQUE, role_id int, gender VARCHAR(1), super_id int, PRIMARY KEY(emp_id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Employee table created... ');
+    });
+});
+
+//create role table http://127.0.0.1:3000/createroletable
+
+app.get('/createroletable', (req, res) => {
+    let sql = 'CREATE TABLE role(role_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, position VARCHAR(40))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Roletable created... ');
+    });
+});
+
+//update parameter  http://127.0.0.1:3000/updatedattributes
+app.get('/updatedattributes', (req, res) => {
+    let sql = 'ALTER TABLE employeelist ADD FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE SET NULL';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Attributed updated...');
+    });
+});
+
+
+// Select employee http://127.0.0.1:3000/getemployeelist
+
+app.get('/getemployeelist', (req, res) => {
+    let sql = `SELECT * FROM employeelist WHERE emp_id=1`;
     let query = db.query(sql, (err, results) => {
         if (err) throw err;
         console.log(results);
-        res.send('Posts fetched...');
+        res.send('Employees fetched...');
     });
 });
+
 
 
 // Insert Job Into the Data Base
@@ -58,16 +90,6 @@ app.get('/getposts', (req, res) => {
 //   });
 //});
 
- */
-
-app.listen('3002', () => {
-    console.log('Server started on port 3002');
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
 });
-/* 
-function GetSelectedItem2() {
-
-    data = document.getElementById("firstname").value;
-    document.getElementById('test2').textContent = data;
-
-
-} */
--- Web Design/PHP/index.php
+++ Web Design/PHP/index.php
@@ -1,32 +1,30 @@
 <?php
- $dbhost='localhost:3306'
-
-
-include_once 'includes/dbh.php';
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-</head>
-<body>
-  <?php
-   $sql = "SELECT * FROM employee;";
-   $result = mysqli_query($conn, $sql);
-   $resultCheck = mysqli_num_rows($result);
-
-   if ($resultCheck > 0){
-     while($row = mysqli_fetch_assoc($result)){
-        echo $row['employe_emp_id']. "<br>";
-     }
-
+   $dbhost = 'localhost:3000';
+   $dbuser = 'root';
+   $dbpass = '12345';
+   
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
    }
-
-  ?>
-
-
-</body>
-</html>
-
-<!-- https://www.tutorialspoint.com/php/mysql_select_php.htm check this link to get data from database -->
+   
+   $sql = 'SELECT emp_id, first_name, last_name, emailAddress FROM employee';
+   mysql_select_db('teckglobal');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   
+   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "EMP ID :{$row['emp_id']}  <br> ".
+         "First Name : {$row['first_name']} <br> ".
+         "Last Name : {$row['last_name']} <br> ".
+         "Email : {$row['emailAddress']} <br> ";
+   }
+   
+   echo "Fetched data successfully\n";
+   
+   mysql_close($conn);
+?>
